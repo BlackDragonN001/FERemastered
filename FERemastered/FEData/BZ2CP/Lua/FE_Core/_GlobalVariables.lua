2ac5fc117a0bdb7ba3cf73196c392fe3290d4a36
JJ173 <jack.t371@hotmail.co.uk>
2019-03-03 19:53:18 +0000

    Added _GlobalVariables.lua and modified _MPI to match.

---
+++ FERemastered/FEData/BZ2CP/Lua/FE_Core/_GlobalVariables.lua
@@ -0,0 +1,37 @@
+local _GlobalVariables = {};
+
+-- TEAM RELATIONSHIPS
+_GlobalVariables.TEAMRELATIONSHIP_INVALIDHANDLE = 0 -- One or both handles is invalid
+_GlobalVariables.TEAMRELATIONSHIP_SAMETEAM = 1 -- Team # for both items is the same
+_GlobalVariables.TEAMRELATIONSHIP_ALLIEDTEAM = 2 -- Team # isn't identical, but teams are allied
+_GlobalVariables.TEAMRELATIONSHIP_ENEMYTEAM = 3 --Team # isn't identical, and teams are enemies
+
+-- TEAM SLOT VARS
+_GlobalVariables.DLL_TEAM_SLOT_RECYCLER = 1
+_GlobalVariables.DLL_TEAM_SLOT_FACTORY = 2
+
+-- NETLIST VARS
+_GlobalVariables.NETLIST_MPVehicles = 0
+_GlobalVariables.NETLIST_StratStarting = 1
+_GlobalVariables.NETLIST_Recyclers = 2
+_GlobalVariables.NETLIST_AIPs = 3
+_GlobalVariables.NETLIST_Animals = 4
+_GlobalVariables.NETLIST_STCTFGoals = 5
+_GlobalVariables.NETLIST_IAHumanRecyList = 6
+_GlobalVariables.NETLIST_IACPURecyclers = 7
+_GlobalVariables.NETLIST_IAAIPs = 8
+
+_GlobalVariables.MAX_TEAMS = 16
+
+_GlobalVariables.PREGETIN_DENY = 0 -- Deny the pilot entry to the craft
+_GlobalVariables.PREGETIN_ALLOW = 1 -- Allow the pilot entry
+
+_GlobalVariables.PRESNIPE_KILLPILOT = 0 -- Kill the pilot (1.0-1.3.6.4 default). Does still pass this to bullet hit code, where damage might also be applied
+_GlobalVariables.PRESNIPE_ONLYBULLETHIT = 1 -- Do not kill the pilot. Does still pass this to bullet hit code, where damage might also be applied
+
+_GlobalVariables.DoEjectPilot = 0 -- Do 'standard' eject
+_GlobalVariables.DoRespawnSafest = 1 -- Respawn a 'PLAYER' at safest spawnpoint
+_GlobalVariables.DLLHandled = 2 -- DLL handled actions. Do nothing ingame
+_GlobalVariables.DoGameOver = 3 -- Game over, man.
+
+return _GlobalVariables;
