c9576b8f7ff96d12b934f3c4cb6faa05be2e4071
jack775544 <jack775544@gmail.com>
2020-02-27 19:22:10 +1000

    Updated install script to theoretically support Windows 7

--- install.ps1
+++ install.ps1
@@ -44,27 +44,76 @@ Param(
 	$FeRepoDirectory = "."
 )
 
-# Make the new FE directory if it does not already exist
-if (-not (Test-Path $FeInstallDirectory)) {
-	New-Item -Type Directory $FeInstallDirectory
+function New-Symlink {
+	Param($Source, $Target)
+
+	Write-Verbose "Symlink Source: $Source"
+	Write-Verbose "Symlink Target: $Target"
+
+	# Need to check if it is a file or a folder to pass to mklink
+	if ((Get-Item $Source).PSIsContainer) {
+		cmd /c mklink /D $Target $Source > $null
+	} else {
+		cmd /c mklink $Target $Source > $null
+	}
 }
 
-# Symlink steam battlezone install into new location
-# We will symlink each top level thing individually to give granular control over the items
-Get-ChildItem $BzInstallDirectory -Exclude "bzone.cfg" | ForEach-Object {
-	New-Item -Type SymbolicLink -Target $_.FullName "$FeInstallDirectory\$($_.Name)"
+function Invoke-FESetup {
+	[CmdletBinding()]
+	Param($FeInstallDirectory, $BzInstallDirectory, $FeRepoDirectory)
+
+	# Make the new FE directory if it does not already exist
+	if (-not (Test-Path $FeInstallDirectory)) {
+		New-Item -Type Directory $FeInstallDirectory > $null
+	}
+
+	# Now we know that every folder should exist, resolve any relative paths
+	$FeInstallDirectory = Resolve-Path $FeInstallDirectory
+	$BzInstallDirectory = Resolve-Path $BzInstallDirectory
+	$FeRepoDirectory = Resolve-Path $FeRepoDirectory
+
+	Write-Verbose $FeInstallDirectory
+	Write-Verbose $BzInstallDirectory
+	Write-Verbose $FeRepoDirectory
+
+	Write-Host "Creating links to Battlezone install..."
+
+	# Symlink steam battlezone install into new location
+	# We will symlink each top level thing individually to give granular control over the items
+	Get-ChildItem $BzInstallDirectory -Exclude "bzone.cfg" | ForEach-Object {
+		# New-Item -Type SymbolicLink -Target $_.FullName "$FeInstallDirectory\$($_.Name)"
+		New-Symlink $_.FullName "$FeInstallDirectory\$($_.Name)"
+	}
+
+	Write-Host "Copying battlezone config files..."
+	Copy-Item $BzInstallDirectory\bzone.cfg $FeInstallDirectory\bzone.cfg
+
+	Write-Host "Creating links to Forgotten Enemies install..."
+	New-Symlink $FeRepoDirectory $FeInstallDirectory\FERemastered
+
+	Write-Host "Copying Forgotten Enemies config..."
+	Copy-Item $FeRepoDirectory\FE_RM_Config\FERM.cfg $FeInstallDirectory\FERM.cfg
+	Copy-Item $FeRepoDirectory\FE_RM_Config\FERM_bake.cfg $FeInstallDirectory\FERM_bake.cfg
+	Copy-Item $FeRepoDirectory\steam_appid.txt $FeInstallDirectory\steam_appid.txt
+
+	Write-Host "Creating batch shortcuts..."
+	"start .\battlezone2.exe /nointro /config FERM.cfg" | Out-File -Encoding ascii $FeInstallDirectory\bzone-fe.bat
+	"start .\battlezone2.exe /nointro /config FERM.cfg /noscript" | Out-File -Encoding ascii $FeInstallDirectory\bzone-fe-noscript.bat
+	"start .\battlezone2.exe /nointro /config FERM_bake.cfg" | Out-File -Encoding ascii $FeInstallDirectory\bzone-fe-bake.bat
+
+	Write-Host
+	Write-Host -ForegroundColor Yellow @"
+Finished installing Forgotten Enemies.
+
+To play the mod head to '$FeInstallDirectory' and run 'bzone-fe.bat'
+"@
 }
 
-# Copy specific files
-Copy-Item $BzInstallDirectory\bzone.cfg $FeInstallDirectory\bzone.cfg
-Copy-Item $FeRepoDirectory\steam_appid.txt $FeInstallDirectory\steam_appid.txt
+$args = @{
+	FeInstallDirectory = $FeInstallDirectory
+	BzInstallDirectory = $BzInstallDirectory
+	FeRepoDirectory = $FeRepoDirectory
+	ErrorAction = 'Stop'
+}
 
-# Symlink the fe repo into addon of the game directory
-New-Item -Type SymbolicLink -Target $FeRepoDirectory $FeInstallDirectory\FERemastered
-
-Copy-Item $FeRepoDirectory\FE_RM_Config\FERM.cfg $FeInstallDirectory\FERM.cfg
-Copy-Item $FeRepoDirectory\FE_RM_Config\FERM_bake.cfg $FeInstallDirectory\FERM_bake.cfg
-
-"start .\battlezone2.exe /nointro /config FERM.cfg" | Out-File -Encoding ascii $FeInstallDirectory\bzone-fe.bat
-"start .\battlezone2.exe /nointro /config FERM.cfg /noscript" | Out-File -Encoding ascii $FeInstallDirectory\bzone-fe-noscript.bat
-"start .\battlezone2.exe /nointro /config FERM_bake.cfg" | Out-File -Encoding ascii $FeInstallDirectory\bzone-fe-bake.bat
+Invoke-FESetup @args
