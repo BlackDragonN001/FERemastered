b6822dfe2140782e406d978708f7b1204a77ede4
JJ173 <94978675+JJ173@users.noreply.github.com>
2025-05-09 21:40:33 +0100

    Formatted IA / MPI Lua Code

--- baked/FE_RM_Config/FERemastered/BZ2CP/Lua/InstantAction/Instant.lua
+++ baked/FE_RM_Config/FERemastered/BZ2CP/Lua/InstantAction/Instant.lua
@@ -1,12 +1,12 @@
---[[ 
-	Summary: BZCC FE Instant Action Mission Script 
+--[[
+	Summary: BZCC FE Instant Action Mission Script
 	Author: JJ (AI_Unit)
-	Version: 1.0 
-	Last modified: 21/09/2022 
+	Version: 1.0
+	Last modified: 21/09/2022
 --]]
 
 -- File find fix.
-assert(load(assert(LoadFile("_requirefix.lua")),"_requirefix.lua"))();
+assert(load(assert(LoadFile("_requirefix.lua")), "_requirefix.lua"))();
 
 -- Load the core files.
 local _FECore = require('_FECore');
@@ -25,55 +25,54 @@ local endDelta = 10.0;
 local debug = true;
 
 -- IA Starting Vehicle handles.
-local StartingVehicleTable = 
+local StartingVehicleTable =
 {
-    {"vturr", "vturr", "vscav"},
-    {"vturr", "vturr", "vscav", "vtank", "vscout", "vscout"},
-    {"vturr", "vturr", "vscav", "vtank", "vtank", "vtank", "vscout", "vscout", "vscout"},
+	{ "vturr", "vturr", "vscav" },
+	{ "vturr", "vturr", "vscav", "vtank", "vscout", "vscout" },
+	{ "vturr", "vturr", "vscav", "vtank", "vtank",  "vtank", "vscout", "vscout", "vscout" },
 }
 
-local Mission = 
+local Mission =
 {
 	m_ElapsedGameTime = 0, -- turn counter.
-	m_HumanRace = 'i', 	-- CPU Race.
+	m_HumanRace = 'i',  -- CPU Race.
 	--m_CPURace = 'i', 	-- CPU Race.
-    --m_Difficulty = 0,     -- Chosen in IA shell.
-	m_RespawnEnabled = 0, 	-- Chosen in IA shell.
-    m_HumanStartingForceSize = 1,     -- Starting force size for humans.
-    --m_CPUStartingForceSize = 1,     -- Start force size for CPU.
-    m_GameOver = false,     -- Handle game over.
+	--m_Difficulty = 0,     -- Chosen in IA shell.
+	m_RespawnEnabled = 0,      -- Chosen in IA shell.
+	m_HumanStartingForceSize = 1, -- Starting force size for humans.
+	--m_CPUStartingForceSize = 1,     -- Start force size for CPU.
+	m_GameOver = false,        -- Handle game over.
 	--m_SetFirstAIP = false,     -- For SetPlan() taunts.
-    --m_PlayerRespawnEnabled = false,     -- If player respawn is enabled.
+	--m_PlayerRespawnEnabled = false,     -- If player respawn is enabled.
 	--m_SentFirstCPUScavToPool = false, 	-- To help the CPU, this flag will send the first spawned CPU Scav to a pool before Recycler deployment.
-	m_BuildingStartingUnits = false, 	-- Halt the dispatcher until we're ready. This must be Saved so things will work correctly on a Load.
-	m_EnemyRecycler = nil, -- CPU Recycler.
-	m_EnemyRecycler2 = nil, -- If it's 3 way.
-	m_Recycler = nil, -- Human Recycler.
-	m_MapPools = {}, 	-- Keep track of the maps pools.
+	m_BuildingStartingUnits = false, -- Halt the dispatcher until we're ready. This must be Saved so things will work correctly on a Load.
+	m_EnemyRecycler = nil,        -- CPU Recycler.
+	m_EnemyRecycler2 = nil,       -- If it's 3 way.
+	m_Recycler = nil,             -- Human Recycler.
+	m_MapPools = {},              -- Keep track of the maps pools.
 }
 
 -- Save game data.
 function Save()
-    return _FECore.Save(), _MPI.Save(), Mission;
+	return _FECore.Save(), _MPI.Save(), Mission;
 end
 
 -- Load game data.
 function Load(FECoreData, MPIData, MissionData)
-
-    -- Don't auto group units.
+	-- Don't auto group units.
 	SetAutoGroupUnits(false);
 
 	-- Want to trigger ObjectKilled for AI units.
 	WantBotKillMessages();
-	
+
 	-- Load sub modules.
 	_FECore.Load(FECoreData);
-	
+
 	_MPI.Load(MPIData);
 
 	-- Load mission data.
 	Mission = MissionData;
-	
+
 	-- Do this for everyone as well.
 	CreateObjectives();
 end
@@ -86,38 +85,36 @@ end
 
 -- Pre-game initial setup.
 function InitialSetup()
-
-    -- Initiate sub modules.
-    _FECore.InitialSetup();
+	-- Initiate sub modules.
+	_FECore.InitialSetup();
 	_MPI.InitialSetup();
 
-    -- Don't auto group units.
+	-- Don't auto group units.
 	SetAutoGroupUnits(false);
 
 	-- Want to trigger ObjectKilled for AI Units.
 	WantBotKillMessages();
-	
 end
 
 -- Handle when an object is added to the world.
-function AddObject(h) 
-    -- Call core AddObject method.
-    _FECore.AddObject(h);
+function AddObject(h)
+	-- Call core AddObject method.
+	_FECore.AddObject(h);
 
-    -- Grab our important details.
-    local teamNum = GetTeamNum(h);
+	-- Grab our important details.
+	local teamNum = GetTeamNum(h);
 	local ODFName = GetCfg(h);
 	local ObjClass = GetClassLabel(h);
 
 	-- Player stuff.
 	if (teamNum == PLAYER_START_TEAM) then
 		SetSkill(h, 3);
-    -- Check CPU stuff.
-    elseif (teamNum >= CPU_START_TEAM) then
+		-- Check CPU stuff.
+	elseif (teamNum >= CPU_START_TEAM) then
 		-- CPU "Commander" pilots.
 		if (string.sub(ODFName, 3, 9) == "cmdr_c") then
 			SetSkill(h, 3); -- Set Commander maximum skill to 3.
-		else 
+		else
 			-- Give the CPU a custom pilot config.
 			if (not IsBuilding(h) and string.sub(ODFName, 1, 1) ~= "c") then
 				SetPilotClass(h, string.sub(ODFName, 1, 1) .. "spilo_c");
@@ -137,10 +134,9 @@ function AddObject(h)
 			table.insert(Mission.m_MapPools, h);
 		end
 	end
-	
+
 	-- Call MPI addobject.
 	_MPI.AddObject(h, Mission.m_BuildingStartingUnits, Mission.m_ElapsedGameTime, ODFName, ObjClass);
-	
 end
 
 -- Runs on mission state.
@@ -155,14 +151,14 @@ function Start()
 	-- Get our difficulty.
 	--Mission.m_Difficulty = IFace_GetInteger("shell.instant.difficulty");
 
-    -- Remove any trace of old player vehicles left in the BZN.
+	-- Remove any trace of old player vehicles left in the BZN.
 	local PlayerEntryH = GetPlayerHandle();
 
 	if (PlayerEntryH ~= nil) then
 		RemoveObject(PlayerEntryH);
 	end
 
-    -- Rebuild the player.
+	-- Rebuild the player.
 	local LocalTeamNum = GetLocalPlayerTeamNumber();
 	local PlayerH = SetupPlayer(LocalTeamNum);
 	SetAsUser(PlayerH, LocalTeamNum);
@@ -170,44 +166,42 @@ function Start()
 
 	-- Put up Objectives.
 	CreateObjectives();
-	
+
 	-- Call MPI start.
 	_MPI.Start();
-	
+
 	-- Spawn the CPU.
 	Mission.m_BuildingStartingUnits = true;
 	Mission.m_EnemyRecycler = _MPI.SetupAITeam(CPU_START_TEAM);
-	      
+
 	if (IFace_GetInteger("options.instant.int0") ~= 0) then
 		Mission.m_EnemyRecycler2 = _MPI.SetupAITeam(CPU_START_TEAM + CPU2_TEAM_OFFSET);
 	end
-	
+
 	Mission.m_BuildingStartingUnits = false;
 end
 
 -- Called per tick.
 function Update()
-    -- Call core AddObject method.
-    _FECore.Update();
+	-- Call core AddObject method.
+	_FECore.Update();
 
 	-- turn counter.
 	Mission.m_ElapsedGameTime = Mission.m_ElapsedGameTime + 1;
-	
+
 	-- Call MPI update.
 	_MPI.Update(Mission.m_ElapsedGameTime);
-	
+
 	-- Test win conditions.
 	TestGameOver();
-	
 end
 
 -- Handle win conditions.
 function TestGameOver()
 	if (not Mission.m_GameOver) then
-	
 		-- Loop over AI teams
 		local AITeamsAlive = false;
-		for i = CPU_START_TEAM, MAX_TEAMS-1 do
+		for i = CPU_START_TEAM, MAX_TEAMS - 1 do
 			-- AI Team 1.
 			if i == CPU_START_TEAM then
 				if (Mission.m_EnemyRecycler ~= nil) then
@@ -226,10 +220,10 @@ function TestGameOver()
 						end
 					end
 				end
-			-- AI Team 2
+				-- AI Team 2
 			elseif i == CPU_START_TEAM + CPU2_TEAM_OFFSET then
 				if (Mission.m_EnemyRecycler2 ~= nil) then
-					 if IsAround(Mission.m_EnemyRecycler2) then	
+					if IsAround(Mission.m_EnemyRecycler2) then
 						AITeamsAlive = true;
 						break;
 					else
@@ -246,15 +240,15 @@ function TestGameOver()
 				end
 			end
 		end
-		
+
 		if not AITeamsAlive then
 			SucceedMission(GetTime() + 5.0, "instantw.txt");
 			Mission.m_GameOver = true;
 		end
-		
+
 		if (not IsAround(Mission.m_Recycler)) then
 			local tempH = GetObjectByTeamSlot(PLAYER_START_TEAM, DLL_TEAM_SLOT_RECYCLER);
-			
+
 			if (tempH ~= 0) then
 				Mission.m_Recycler = tempH; -- Save it.
 			else
@@ -274,8 +268,8 @@ function SetupPlayer()
 	-- Lua arrays start with an index of 1, not 0, so add + 1 for later use.
 	Mission.m_HumanStartingForceSize = IFace_GetInteger("options.instant.playerforce") + 1;
 
-    -- This player is their own commander; set up their equipment.
-    local spawnpointPosition = GetPosition("Recycler");
+	-- This player is their own commander; set up their equipment.
+	local spawnpointPosition = GetPosition("Recycler");
 	spawnpointPosition.y = TerrainFindFloor(spawnpointPosition.x, spawnpointPosition.z) + 2.5;
 
 	-- Create the player.
@@ -288,7 +282,8 @@ function SetupPlayer()
 	local chosenPlayerRecy = IFace_GetString("options.instant.string1");
 	chosenPlayerRecy = Mission.m_HumanRace .. chosenPlayerRecy:sub(2);
 
-	Mission.m_Recycler = BuildObject(chosenPlayerRecy, PLAYER_START_TEAM, GetPositionNear(spawnpointPosition, VEHICLE_SPACING_DISTANCE * 1.25, VEHICLE_SPACING_DISTANCE * 1.25));
+	Mission.m_Recycler = BuildObject(chosenPlayerRecy, PLAYER_START_TEAM,
+		GetPositionNear(spawnpointPosition, VEHICLE_SPACING_DISTANCE * 1.25, VEHICLE_SPACING_DISTANCE * 1.25));
 	SetGroup(Mission.m_Recycler, 0);
 
 	-- Spawn extra Human vehicles.
@@ -322,7 +317,6 @@ function PlayerDied(deadObjectHandle, sniped)
 
 	-- Local player got killed (pilot or snipe). Do respawn as needed.
 	if (Mission.m_RespawnEnabled) and IsAround(Mission.m_Recycler) then
-	
 		-- Respawn them near their m_Recycler, up in the air.	
 		local Where = GetPosition(Mission.m_Recycler, Where);
 
@@ -334,7 +328,7 @@ function PlayerDied(deadObjectHandle, sniped)
 		SetAsUser(PlayerH, 1);
 		AddPilotByHandle(PlayerH);
 
-		DoTaunt(TAUNTS_HumanShipDestroyed);		
+		DoTaunt(TAUNTS_HumanShipDestroyed);
 	else
 		-- User can't respawn. Game over, man
 		FailMission(GetTime() + 3);
@@ -352,7 +346,7 @@ function ObjectKilled(deadObjectHandle, killersHandle)
 		if (not IsPerson(deadObjectHandle)) then
 			return _FECore.ObjectKilled(deadObjectHandle, killersHandle) or DoEjectRatio(deadObjectHandle); --EJECTKILLRETCODES_DOEJECTPILOT; -- Return DoEjectPilot.
 		else
-			return EJECTKILLRETCODES_DLLHANDLED; -- Return DLLHandled.
+			return EJECTKILLRETCODES_DLLHANDLED;                                                   -- Return DLLHandled.
 		end
 	else
 		-- Handle player death.
@@ -369,23 +363,15 @@ function ObjectSniped(deadObjectHandle, killersHandle)
 	end
 end
 
-
-
-
-
-
-
-
-
 -- TODO: Move to core to be used universally?
 function SpawnTeamExtraVehicles(Team, Race, Pos, Force)
 	-- Get the correct amount of units to spawn based on Force parameter.
 	local vehicles = StartingVehicleTable[Force];
-	
+
 	Mission.m_BuildingStartingUnits = true;
 
 	-- Loop through each unit to handle their placement.
-	for i, v in pairs (vehicles) do
+	for i, v in pairs(vehicles) do
 		-- Get each handle within the table.
 		local h = vehicles[i];
 
@@ -399,7 +385,7 @@ function SpawnTeamExtraVehicles(Team, Race, Pos, Force)
 
 		-- Generate a position around given Pos vector.
 		local pos = GetPositionNear(Pos, VEHICLE_SPACING_DISTANCE * 1.25, VEHICLE_SPACING_DISTANCE * 1.25);
-		
+
 		-- Finally, build it!
 		local vehicle = BuildObject(h, Team, pos);
 	end
